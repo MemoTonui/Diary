898fb1e45a4e98a7b599b4653cb74f9152617358
Linda Tonui <tonuilinda@gmail.com>
2020-07-31 18:32:51 +0300

    Add home page

--- app/src/main/java/com/moringa/diary/MainActivity.java
+++ app/src/main/java/com/moringa/diary/MainActivity.java
@@ -2,13 +2,19 @@ package com.moringa.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/happy.ttf");
+        textView = (TextView)findViewById(R.id.textView);
     }
 }
