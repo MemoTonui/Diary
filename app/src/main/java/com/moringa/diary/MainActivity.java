1687ba0d5fced1804776ff524059a1b6ccdabad9
Linda Tonui <tonuilinda@gmail.com>
2020-08-01 13:17:46 +0300

    Add sign up page

--- app/src/main/java/com/moringa/diary/MainActivity.java
+++ app/src/main/java/com/moringa/diary/MainActivity.java
@@ -2,20 +2,35 @@ package com.moringa.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    @BindView
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView (R.id.button) Button mbutton;
     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/happy.ttf");
         textView = (TextView)findViewById(R.id.textView);
+
+        mbutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this,SignUp.class) ;
+        startActivity(intent);
     }
 }
