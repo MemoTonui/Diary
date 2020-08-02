4cbb9535de9a6a6770b579bb8a82eb231ceef18b
Linda Tonui <tonuilinda@gmail.com>
2020-08-02 15:23:00 +0300

    Add date functionalities

--- app/src/main/java/com/moringa/diary/Page1.java
+++ app/src/main/java/com/moringa/diary/Page1.java
@@ -1,8 +1,10 @@
 package com.moringa.diary;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class Page1 extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class Page1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page1);
 
+
+    }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
 }
