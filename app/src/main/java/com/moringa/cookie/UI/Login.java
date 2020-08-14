0a81478e96559c34da8e26b2bf0e139fb0c7eb83
Linda Tonui <tonuilinda@gmail.com>
2020-08-14 17:56:25 +0300

    Add create account authentication

--- app/src/main/java/com/moringa/cookie/UI/Login.java
+++ app/src/main/java/com/moringa/cookie/UI/Login.java
@@ -16,14 +16,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
+
     @BindView(R.id.loginbutton)
     Button mLoginbutton;
-    @BindView(R.id.name2)
-    EditText mName2;
+    @BindView(R.id.password)
+    EditText mPassword;
     @BindView(R.id.email2)
     EditText mEmail2;
     @BindView(R.id.invalid)
     TextView mInvalid;
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
 
     @Override
@@ -37,10 +39,16 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+         if(view == mRegisterTextView){
+             Intent intent = new Intent(Login.this,SignUp.class);
+             startActivity(intent);
+             finish();
+         }
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         String email = intent.getStringExtra("email");
-        String name2=mName2.getText().toString();
+       // String name2=mName2.getText().toString();
         String email2 =mEmail2.getText().toString();
 
        /* if (name != name2 || email != email2) {
@@ -50,7 +58,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         } else {*/
 
             Intent intent1 = new Intent(Login.this, Page1.class);
-            Toast.makeText(Login.this, "Hi " + name2 + " !! You have successfully Logged in", Toast.LENGTH_LONG).show();
+           // Toast.makeText(Login.this, "Hi " + name2 + " !! You have successfully Logged in", Toast.LENGTH_LONG).show();
             startActivity(intent1);
         }
 
